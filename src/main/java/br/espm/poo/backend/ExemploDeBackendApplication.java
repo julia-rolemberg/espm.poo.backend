c66f945f69412f73ab741bcc2a7c54d359d8566d
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-05-06 10:20:38 -0300

    primeiro commit

---
+++ src/main/java/br/espm/poo/backend/ExemploDeBackendApplication.java
@@ -0,0 +1,13 @@
+package br.espm.poo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploDeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploDeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/espm/poo/backend/ExemploDeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.poo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploDeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
